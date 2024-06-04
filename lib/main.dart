c5267029b8f183b0a047ea416cc33d6bc4935fbf
Prakhar Verma <connect@iprakharv.com>
2024-06-04 17:03:59 +0530

    Implemented chatmodel

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
-  runApp(const MainApp());
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterwhatsapp/whatsapp_home.dart';
+
+List<CameraDescription> cameras;
+
+Future<Null> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(new MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return new MaterialApp(
+      title: "WhatsApp",
+      theme: new ThemeData(
+        primaryColor: new Color(0xff075E54),
+        accentColor: new Color(0xff25D366),
       ),
+      debugShowCheckedModeBanner: false,
+      home: new WhatsAppHome(cameras:cameras),
     );
   }
 }
